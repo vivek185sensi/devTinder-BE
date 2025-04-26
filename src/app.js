d471a77c952cbab0fbe234e7d2c92e04be80e68c
vivek185 <rampellyvivek185@gmail.com>
2025-04-26 13:29:38 +0530

    routing and request ahsndlers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,42 @@
 const express=require('express');
 const app=express()
 
-app.use('/',(req,res)=>{
-    res.send('Hello Nodejs app')
+
+app.post('/local/boy',(req,res)=>{
+    res.send('sub local')
 })
 
-app.use('/local',(req,res)=>{
+
+app.get('/local',(req,res)=>{
     res.send('i am local')  
+})
+
+app.delete('local',(req,res)=>{
+    res.send('finall delete')
+})
+
+app.get(/.*fly$/,(req,res)=>{
+    res.send('this is regex');
+})
+
+app.get('n?w',(req,res)=>{
+    res.send('this is question')
+})
+
+app.get('no*w',(req,res)=>{
+    res.send('this is multiples')
+})
+
+app.get('som+e',(req,res)=>{
+    res.send('this is add ')
+})
+
+app.get('e(ve)+n',(req,res)=>{
+    res.send('this is group ')
+})
+
+app.use('/',(req,res)=>{
+    res.send('Hello Nodejs app')
 })
 
 app.listen(5000,()=>{
